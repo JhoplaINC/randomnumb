70f0df15918f51341a85825942bbcf2cba86db10
JhoplaINC <34752848+JhoplaINC@users.noreply.github.com>
2019-07-29 05:41:36 -0400

    Add files via upload

---
+++ randomnumb.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+print("\nIngrese el número del cual quiera saber cuantas combinaciones posibles tiene:")
+num=int(input())
+print("\n**************")
+num=(num-1)*num
+print("La cantidad de combinaciones posibles es: "+str(num))
+print("**************")
